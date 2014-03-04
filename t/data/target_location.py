c6013facff6c1a4589332f7e1c95928a44e11d55
tek <torstenschmits@gmail.com>
2014-03-04 16:30:20 +0100

    remove whitespace

--- t/data/target_location.py
+++ t/data/target_location.py
@@ -18,35 +18,35 @@ from fourthparty.sub import (LoooooooooooooooooooongAssName, Anoooother,
 foo = 1
 
 class Test(object):
-    
+
     def dummy(self):
         5 == 2 * 3
-    
+
     def dummy(self):
         5 == 2 * 3
-    
+
     def dummy(self):
         5 == 2 * 3
-    
+
     def dummy(self):
         5 == 2 * 3
-    
+
     def dummy(self):
         5 == 2 * 3
 
 class Test(object):
-    
+
     def dummy(self):
         5 == 2 * 3
-    
+
     def dummy(self):
         5 == 2 * 3
-    
+
     def dummy(self):
         5 == 2 * 3
-    
+
     def dummy(self):
         5 == 2 * 3
-    
+
     def dummy(self):
         5 == 2 * 3
